e4b1a8a6baf3365391f421509ae6a55aec571e09
mikedug141 <90247782+mikedug141@users.noreply.github.com>
2021-09-27 09:07:11 +0700

    first

--- main.js
+++ main.js
@@ -1,31 +1,48 @@
-function toast ( {
-    title = '';
-    message = '';
-    type = 'info';
-    duration = 3000;
-} ) {
-    const main = document.getElementsById('toast');
+function toast({
+    title = '',
+    message = '',
+    type = '',
+    duration = 3000,
+}) {
+    const main = document.getElementById('toast');
     if (main) {
         const toast = document.createElement('div');
-        toast.classList.add('toast');
+        
+        main.appendChild(toast);
+
+        const autoRemoved = setTimeout(function() {
+        main.removeChild(toast);      // ấn x đê tắt
+        }, duration + 1000)
+
+        toast.onclick = function(e) {
+            if(e.target.closest('.toast_close')) {
+                main.removerChild(toast);
+            }
+        }
+        const delay = (duration / 1000).toFixed(2);
+        toast.classList.add('toast', `toast__${type}`);
+        toast.style.animation = ` animation: slideInLeft ease .3s, fadeOut linear 1s ${delay}s forwards;`
         toast.innerHTML = `
-        <div class="toast__icon">;
-        <i class="fas fa-check"></i>;
-            </div>
-            <div class="toast__body">
-                <h3 class="toast__title">${title} </h3>
-                <p class="toast__msg">${message}</p>
-            </div>
-            <div class="toast__close">
-                <i class="fas fa-times"></i>
-                </div> 
-                `;
-                main.appendChild(toast);
+        <div class="toast__icon">
+        <i class="fas fa-check"></i>
+        </div>
+        <div class="toast__body">
+        <h3 class='toast__title'>${title}</h3>
+        <p class='toast__msg'>${message}</p>
+        </div>
+        <div class='toast__close'>
+        <i class="fas fa-times"></i>
+        </div>
+        `;
+
     }
 }
-toast ({
-    title: 'success';
-    message: 'vô đi nhó';
-    type: 'success';
-    duration: 3000
-});
+
+function showSuccessToast() {
+    toast({
+    title: 'Success',
+    message: "đã kết nối thành công vào internet",
+    type: 'success',
+    duration: 5000,
+    })
+}
