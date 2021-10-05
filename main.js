d648843b4344ea9892ed025478bab9bda4d24544
mikedug141 <sonym21402@gmail.com>
2021-10-05 18:10:13 +0700

    first

--- main.js
+++ main.js
@@ -4,22 +4,27 @@ function toast({
     type = '',
     duration = 3000,
 }) {
+    // nhập toast vào hàm main
     const main = document.getElementById('toast');
     if (main) {
         const toast = document.createElement('div');
         
         main.appendChild(toast);
 
-        const autoRemoved = setTimeout(function() {
-        main.removeChild(toast);      // ấn x đê tắt
-        }, duration + 1000)
+// auto remove
+const autoRemoved = setTimeout(function() {
+    main.removeChild(toast);     
+    }, duration + 1000)
 
+ // click x to remove
         toast.onclick = function(e) {
-            if(e.target.closest('.toast_close')) {
-                main.removerChild(toast);
+            if(e.target.closest('.toast__close')) {
+                main.removeChild(toast);
             }
         }
         const delay = (duration / 1000).toFixed(2);
+
+        // thêm hàm HTML 
         toast.classList.add('toast', `toast__${type}`);
         toast.style.animation = ` animation: slideInLeft ease .3s, fadeOut linear 1s ${delay}s forwards;`
         toast.innerHTML = `
